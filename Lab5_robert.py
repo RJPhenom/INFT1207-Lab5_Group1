f7b0a176ce0a8b1820202f3f74d17f9bdf59f490
RJ <rj@macklem.ca>
2024-03-28 14:30:20 -0400

    Add: comment header
    Added comment header to main py file

---
+++ Lab5_robert.py
@@ -0,0 +1,8 @@
+# *************************************************************************************
+# Title:        Lab 5 Selenium WebDriver Women's Shopping Test Script
+# Author:       Robert Macklem
+# Date:         March 28 2024
+# Description:  Tests website functionality at https://magento.softwaretestingboard.com
+#               using Selenium WebDriver
+# *************************************************************************************
+
